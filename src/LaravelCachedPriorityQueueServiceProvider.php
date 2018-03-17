7839e8904fdd2f480b24ceaf2426728b61749db3
David Cheung <david.cheung@continuumeconomics.com>
2018-03-17 14:29:49 -0400

    service provider - empty

---
+++ src/LaravelCachedPriorityQueueServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DavidCheung\LaravelCachedPriorityQueue;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCachedPriorityQueueServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
